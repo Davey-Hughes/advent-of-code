f4a4af33ce4c88bc8c23c78834e28c3de20d74a7
Davey Hughes <davidralphhughes@gmail.com>
2024-01-20 03:40:52 -0800

    Add day 9 part 2

--- 2023/9/src/main.rs
+++ 2023/9/src/main.rs
@@ -1,47 +1,39 @@
+#![feature(array_windows)]
 use std::{env, error::Error, fs, process::exit};
 
-fn parse_input<S: AsRef<str>>(
-    file_string: &S,
-) -> impl Iterator<Item = impl Iterator<Item = i64> + '_> + '_ {
+fn parse_input<S: AsRef<str>>(file_string: &S) -> impl Iterator<Item = Vec<i64>> + Clone + '_ {
     file_string
         .as_ref()
         .lines()
         .map(|s| s.split(' '))
-        .map(|x| x.map(|x| x.parse::<i64>().unwrap()))
+        .map(|x| x.map(|x| x.parse::<i64>().unwrap()).collect())
 }
 
-fn predict_next<I>(history: I) -> i64
-where
-    I: Iterator<Item = i64>,
-{
-    let history_vec = history.collect::<Vec<_>>();
-    let mut sum = *history_vec.last().unwrap();
-    let mut cur_vec = history_vec;
+fn predict_next(history: &[i64]) -> i64 {
+    let next_vec = history
+        .array_windows()
+        .map(|[x, y]| y - x)
+        .collect::<Vec<_>>();
 
-    loop {
-        let mut next_vec = vec![];
-        let mut should_break = true;
-
-        for i in 0..cur_vec.len() - 1 {
-            let k = cur_vec[i + 1] - cur_vec[i];
-
-            if k != 0 {
-                should_break = false;
-            }
-
-            next_vec.push(k);
-        }
-
-        sum += next_vec.last().unwrap();
-
-        if should_break {
-            break;
-        }
-
-        cur_vec = next_vec;
+    // base case
+    if next_vec.iter().all(|&x| x == 0) {
+        return *history.last().unwrap();
     }
 
-    sum
+    history.last().unwrap() + predict_next(&next_vec)
+}
+
+fn predict_prev(history: &[i64]) -> i64 {
+    let next_vec = history
+        .array_windows()
+        .map(|[x, y]| y - x)
+        .collect::<Vec<_>>();
+
+    if next_vec.iter().all(|&x| x == 0) {
+        return history[0];
+    }
+
+    history[0] - predict_prev(&next_vec)
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -53,9 +45,14 @@ fn main() -> Result<(), Box<dyn Error>> {
     }
 
     let contents = fs::read_to_string(&args[1])?;
-    let lines = parse_input(&contents);
 
-    println!("Part 1: {}", lines.map(predict_next).sum::<i64>());
+    let lines = parse_input(&contents);
+    println!(
+        "Part 1: {}",
+        lines.clone().map(|v| predict_next(&v)).sum::<i64>()
+    );
+
+    println!("Part 1: {}", lines.map(|v| predict_prev(&v)).sum::<i64>());
 
     Ok(())
 }
